bae2805753358f338cdc37d1d6807452f10b04a3
Ojochogwu Dickson <07afrik@gmail.com>
2023-12-05 17:52:07 -0800

    initial commit

---
+++ components/et-moi.tsx
@@ -0,0 +1,12 @@
+'use-client'
+import React from 'react'
+
+export default function etmoi() {
+  return (
+    <section className=''>
+      <p>
+        Self-taught Software Enginner building softwares for Startups and Corporations.
+      </p>
+    </section>
+  )
+}
---
+++ lib/da.ts
@@ -0,0 +1,34 @@
+
+export const links = [
+    {
+        name: 'About',
+        extend: '#about'
+    },
+    {
+        name: 'Experience',
+        extend: '#experience'
+    },
+    {
+        name: 'Projects',
+        extend: '#projects'
+    }
+] as const
+
+export const skills = [
+    {
+
+    }
+] as const
+
+export const projects = [
+    {
+
+    }
+] as const
+
+export const experiences = [
+    {
+
+    }
+] as const
+
