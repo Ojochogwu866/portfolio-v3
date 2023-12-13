4fc052ea777c53b37f3ec9e9bc82e876d25b6343
Ojochogwu Dickson <07afrik@gmail.com>
2023-12-13 13:17:58 -0800

    dsp-tb

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -21,7 +21,7 @@ export default function Topbar() {
      initial="hidden"
      viewport={{ once: true, amount: 0.25 }}
      whileInView="show"
-      className='px-[40px] md:px-[80px] pt-3 md:pt-6 flex w-full items-end justify-end md:fixed'>
+      className='px-[40px] md:px-[80px] pt-3 md:pt-6 flex w-full items-end justify-end relative md:fixed'>
         <button onClick={handleClick} className=' z-50 md:py-2 py-1 md:px-8 px-4 flex items-center border-solid border-[1.5px] text-[#6E6A85] border-[#95A1F9] bg-[#0c0a1b] rounded-full'>
           Resume
         </button>
