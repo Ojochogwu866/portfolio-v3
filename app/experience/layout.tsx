c002a873538555c0ff5cc4f037a6c24ae2a3f6f5
Ojochogwu Dickson <07afrik@gmail.com>
2024-02-22 19:07:39 +0100

    WIP--3

--- app/experience/layout.tsx
+++ app/experience/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: 'An overview of my Projects nad Relevant Experiences in recent years. This list may also consist of works I have in Progress',
 }
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode
