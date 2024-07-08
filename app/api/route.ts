bf39b27d757f0d941097771775a5acbd54d84af3
Ojochogwu Dickson <07afrik@gmail.com>
2024-07-08 23:27:19 +0100

    fix: style-bug

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,10 +2,10 @@ import { readFile } from 'fs/promises';
 import path from 'path';
 
 export async function GET() {
-  const buffer = await readFile(path.join(process.cwd(), '/app/assets', 'Ojochogwu-dickson-fullstack-engineer-resume.pdf'));
+  const buffer = await readFile(path.join(process.cwd(), '/app/assets', 'resume.pdf'));
 
   const headers = new Headers();
-  headers.append('Content-Disposition', 'attachment; filename="Ojochogwu-dickson-fullstack-engineer-resume.pdf"');
+  headers.append('Content-Disposition', 'attachment; filename="resume.pdf"');
   headers.append('Content-Type', 'application/pdf');
 
   return new Response(buffer, {
