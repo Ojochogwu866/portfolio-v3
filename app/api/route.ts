15f63eee15edd9f03732aafb535a11e9ba004284
Ojochogwu Dickson <07afrik@gmail.com>
2024-07-08 23:31:58 +0100

    fix: resume path

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,10 +2,10 @@ import { readFile } from 'fs/promises';
 import path from 'path';
 
 export async function GET() {
-  const buffer = await readFile(path.join(process.cwd(), '/app/assets', 'resume.pdf'));
+  const buffer = await readFile(path.join(process.cwd(), '/app/assets', 'Resume.pdf'));
 
   const headers = new Headers();
-  headers.append('Content-Disposition', 'attachment; filename="resume.pdf"');
+  headers.append('Content-Disposition', 'attachment; filename="Resume.pdf"');
   headers.append('Content-Type', 'application/pdf');
 
   return new Response(buffer, {
